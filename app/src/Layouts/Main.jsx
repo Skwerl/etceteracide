77c84b69b9d443b61283d563b56a66e0046ab372
Kevin Cogill <skwerl@gmail.com>
2023-06-30 11:53:53 -0700

    margins

--- app/src/Layouts/Main.jsx
+++ app/src/Layouts/Main.jsx
@@ -1,6 +1,6 @@
 export default function Main(props) {
     const { children } = props;
-    return <div style={{ margin: "20px" }}>
+    return <div style={{ margin: "20px 30px" }}>
         <h1>E T C E T E R A C I D E</h1>
         {children}
     </div>
