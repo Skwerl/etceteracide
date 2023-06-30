320a09048812ccf148383a3f1a7bc679f3618e8c
Kevin Cogill <skwerl@gmail.com>
2023-06-30 11:52:43 -0700

    a layout

---
+++ app/src/Layouts/Main.jsx
@@ -0,0 +1,7 @@
+export default function Main(props) {
+    const { children } = props;
+    return <div style={{ margin: "20px" }}>
+        <h1>E T C E T E R A C I D E</h1>
+        {children}
+    </div>
+}
--- app/src/Pages/Home.jsx
+++ app/src/Pages/Home.jsx
@@ -1,17 +1,15 @@
 import React from 'react';
 import { useGetPostsQuery } from '../Api';
+import Main from '../Layouts/Main';
 
 export default function Home() {
 
   const { data: postsData = null, isLoading: postsLoading } = useGetPostsQuery();
 
-  return <div>
+  return <Main>
     {!postsLoading && <React.Fragment>
-
       <pre>{JSON.stringify(postsData, null, 4)}</pre>
-
     </React.Fragment>}
-
-  </div>
+  </Main>
 
 }
