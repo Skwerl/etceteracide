60db0aa2c5cd81f342a5627548ecc46316ddf784
Kevin Cogill <skwerl@gmail.com>
2023-07-04 16:37:32 -0700

    show date

--- app/src/Pages/Document.jsx
+++ app/src/Pages/Document.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { useGetPostQuery } from '../Redux/Api';
 import Main from '../Layouts/Main';
@@ -7,7 +7,14 @@ import Spinner from '../Components/Spinner';
 export default function Document() {
 
   let { id } = useParams();
+  const [dateString, setDateString] = useState(null);
   const { data: postData = null, isLoading: postLoading } = useGetPostQuery(id);
+
+  useEffect(() => {
+    if (postData && postData.date) {
+      setDateString(new Date(postData.date).toLocaleDateString('en-US'));
+    }
+  }, [postData]);
 
   return <Main>
     <div className="document-wrapper">
@@ -16,6 +23,10 @@ export default function Document() {
         : <React.Fragment>
           <h2>{postData.title}</h2>
           <div dangerouslySetInnerHTML={{ __html: postData.content }} />
+          <small>{dateString
+            ? `Skwerl, ${dateString}`
+            : `Skwerl`
+          }</small>
         </React.Fragment>
       }
     </div>
